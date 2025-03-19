73b3552b67ab22aaf7b64310c39ee57c33bec357
Brunoswag <nostrumlance@gmail.com>
2025-03-19 13:01:42 -0500

    changes at the end of class on 3/19/2025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './child/child.component';
 import { FormsModule } from '@angular/forms';
 
 @Component
-({
-  selector: 'app-root',
-  imports: [RouterOutlet, ChildComponent, FormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent 
-{
+  ({
+    selector: 'app-root',
+    imports: [RouterOutlet, ChildComponent, FormsModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+  })
+export class AppComponent {
   title = 'practice-Component-Interaction';
   pText: string = '';
+
+  receivedChildMessage: String = '';
+  receivedMessage(msg: String) {
+    this.receivedChildMessage = msg;
+  }
+
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-child',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './child.component.html',
   styleUrl: './child.component.css'
 })
-export class ChildComponent
-{
+export class ChildComponent {
 
-  @Input()  inputText: String = 'f';
+  @Input() inputText: String = 'f';
+
+  message: String = '';
+
   @Output() outputValue: EventEmitter<String> = new EventEmitter<String>();
+
+  sendMessage() {
+    this.outputValue.emit(this.message);
+  }
 
 }
